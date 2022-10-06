e78648707234f6d6c758779d5bbb1ce36438cb82
Alan Borsato <37001712+aborsato@users.noreply.github.com>
2022-10-06 18:50:46 +0000

    fix api uri

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
     b2cScopes: ["https://a9oconsulting.onmicrosoft.com/tasks-api/tasks.read", "https://a9oconsulting.onmicrosoft.com/tasks-api/tasks.write"],
-    webApi: "http://localhost:5000/hello"
+    webApi: "https://a9oweb-b2c-api.azurewebsites.net/hello"
   };
